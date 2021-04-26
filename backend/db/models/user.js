bd9d2c8e8a3a3f28aa4a1d9f20e66a8f5384b98f
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-04-26 13:27:44 -0500

    Update associations

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -79,6 +79,8 @@ module.exports = (sequelize, DataTypes) => {
   
   User.associate = function (models) {
     User.hasMany(models.Listing, { foreignKey: 'host_id' });
+    User.hasMany(models.Review, { foreignKey: 'user_id' });
+    User.hasMany(models.Booking, { foreignKey: 'user_id' });
   };
 
   User.prototype.toSafeObject = function () { // remember, this cannot be an arrow function
