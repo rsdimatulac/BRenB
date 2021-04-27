66a70d8abde7911b0472e657e6f4d1aeb53ec68b
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-04-27 13:21:01 -0500

    Added listing and search API routes

---
+++ backend/routes/api/listings.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { Listing } = require('../../db/models');
+
+const router = express.Router();
+
+// GET /api/listings/:id <<--- Get the listing by its ID
+router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+    const listingId = parseInt(req.params.id, 10);
+    const listing = await Listing.findByPk(listingId);
+    return res.json(listing);
+}));
+
+module.exports = router;
