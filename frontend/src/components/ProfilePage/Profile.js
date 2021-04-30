c25d1318c5db39a97f03111bdc1bf156a2f26d7b
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-04-29 23:46:27 -0500

    Working on Profile Page

---
+++ frontend/src/components/ProfilePage/Profile.js
@@ -0,0 +1,11 @@
+import "./Profile.css";
+
+const Profile = () => {
+    return (
+        <div className="profile__container">
+            <h2>Profile Page here</h2>
+        </div>
+    )
+}
+
+export default Profile;
